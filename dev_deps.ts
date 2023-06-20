9768482c11c0cd67f452fae70620eaa9bb34ddec
Duncan Kolba <duncankolba@gmail.com>
2023-06-20 21:56:15 +0200

    feat: 🎸 Use Deno KV for caching responses and etags

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,1 +1,2 @@
 export { assertSnapshot } from "https://deno.land/std@0.188.0/testing/snapshot.ts";
+export { assert } from "https://deno.land/std@0.188.0/testing/asserts.ts";
--- luhmann_test_e2e.ts
+++ luhmann_test_e2e.ts
@@ -1,9 +1,25 @@
-import { assertSnapshot } from "./dev_deps.ts";
+import { assert, assertSnapshot } from "./dev_deps.ts";
 import { parse } from "./deps.ts";
 
 const baseFileUrl = " http://localhost:4507/";
 const baseTestUrl = "http://localhost:8080/";
 const sitemapResource = "sitemap.yaml";
+
+Deno.test("e2e isEtagHit", async function (t): Promise<void> {
+  // Get the homepage the first time
+  const homePageResponse = await fetch(baseTestUrl);
+  // Dispose of the response to prevent a 'leaking resources' error
+  await homePageResponse.body?.cancel();
+  // Extract the etag from header for reuse in the next request
+  const etag = await homePageResponse.headers.get("etag");
+  const headers = { headers: { "If-None-Match": etag ? etag : "NOETAG" } };
+  // Get the homepage the second time, now with If-None-Match header
+  const homePageIfNoneMatch = await fetch(baseTestUrl, headers);
+  // Again, dispose of the response to prevent a 'leaking resources' error
+  await homePageIfNoneMatch.body?.cancel();
+
+  await assert(homePageIfNoneMatch.status === 304);
+});
 
 Deno.test("e2e isSnapshotMatch", async function (t): Promise<void> {
   const homePageResponse = await fetch(baseTestUrl);
